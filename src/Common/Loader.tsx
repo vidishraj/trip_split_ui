7f2bfe26ba41923c7b4009e786a5826ea21ed0eb
vidishraj <vidishraj@gmail.com>
2024-10-05 21:39:55 +0530

    feat: Added Loader in interceptor

---
+++ src/Common/Loader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import { useLoading } from '../Contexts/LoadingContext';
+import Lottie from 'lottie-react';
+import loginAnimation from './loaderAnimation.json';
+const Loader: React.FC = () => {
+  const { loading } = useLoading();
+
+  return loading ? (
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        zIndex: 9999,
+      }}
+    >
+      <Lottie animationData={loginAnimation} style={{ height: 200 }} />
+    </Box>
+  ) : null;
+};
+
+export default Loader;
