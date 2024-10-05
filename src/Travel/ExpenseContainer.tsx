9d4a408b5d51cdcc382902bea44baa9aea1d612e
vidishraj <vidishraj.gmail.com>
2024-10-05 22:33:21 +0530

    refacto: improved styling of empty containers

--- src/Travel/ExpenseContainer.tsx
+++ src/Travel/ExpenseContainer.tsx
@@ -86,15 +86,32 @@ const ExpenseContainer = () => {
         </InfiniteScroll>
       ) : (
         <Box
-          style={{
+          sx={{
             width: '100%',
             height: '100%',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            borderRadius: '12px',
+            boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+            transition: 'all 0.3s ease-in-out',
+            '&:hover': {
+              backgroundColor: 'rgba(255, 255, 255, 0.4)',
+            },
           }}
         >
-          Nothing to see!
+          <Typography
+            variant="h4"
+            sx={{
+              fontSize: '1.5rem',
+              fontWeight: 'bold',
+              color: 'rgba(0, 0, 0, 0.7)',
+              textShadow: '1px 1px 5px rgba(0, 0, 0, 0.2)',
+            }}
+          >
+            Nothing to see!
+          </Typography>
         </Box>
       )}
     </Box>
