82d8eb852193b0aaf4e4253a4a112e214daadf19
vidishraj <vidishraj@gmail.com>
2024-10-17 10:16:26 +0530

    refacto: Added totalExpenditure Card

--- src/Travel/TransactionsContainer.tsx
+++ src/Travel/TransactionsContainer.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography, Card, Avatar, useTheme } from '@mui/material';
 import { useTravel } from '../Contexts/TravelContext';
+import FunctionsIcon from '@mui/icons-material/Functions';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import './BalanceContainer.scss';
 
@@ -8,7 +9,14 @@ const BalanceContainer = () => {
   const travelCtx = useTravel();
   const theme = useTheme();
   const userBalances = travelCtx.state.balances;
-
+  const [totalExpenditure, setTotalExpenditure] = useState(0);
+  useEffect(() => {
+    let totalExpenditureLoc = 0;
+    travelCtx.state.expenses.forEach((item: any) => {
+      totalExpenditureLoc += item.amount;
+    });
+    setTotalExpenditure(totalExpenditureLoc);
+  }, [travelCtx]);
   const getUserName = (userId: number) => {
     const user = travelCtx.state.users.find((u: any) => u.userId === userId);
     return user ? user.userName : 'Unknown';
@@ -31,6 +39,50 @@ const BalanceContainer = () => {
         backgroundColor: 'transparent',
       }}
     >
+      <Card
+        sx={{
+          width: '80%',
+          maxWidth: 400,
+          padding: '16px',
+          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)',
+          borderRadius: '12px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          backdropFilter: 'blur(10px)',
+          backgroundColor: 'rgba(255, 255, 255, 0.6)',
+          [theme.breakpoints.down('sm')]: {
+            padding: '12px',
+          },
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Avatar
+            sx={{
+              marginRight: '12px',
+              backgroundColor: 'orange',
+            }}
+          >
+            <FunctionsIcon />
+          </Avatar>
+          <Typography
+            sx={{
+              fontWeight: 'bold',
+              color: '#333',
+            }}
+          >
+            {'Total'}
+          </Typography>
+        </Box>
+        <Typography
+          sx={{
+            fontWeight: 'bold',
+            color: totalExpenditure >= 0 ? '#4caf50' : '#f44336',
+          }}
+        >
+          ₹ {totalExpenditure.toFixed(2)}
+        </Typography>
+      </Card>
       {Object.keys(userBalances).length > 0 ? (
         Object.entries(userBalances).map(
           ([userId, balance]: [userId: any, balance: any]) => (
@@ -63,17 +115,13 @@ const BalanceContainer = () => {
                 >
                   <AccountBalanceIcon />
                 </Avatar>
-                <Typography
-                  variant="h6"
-                  sx={{ fontWeight: 'bold', color: '#333' }}
-                >
+                <Typography sx={{ fontWeight: 'bold', color: '#333' }}>
                   {getUserName(Number(userId))}
                 </Typography>
               </Box>
 
               {/* Balance */}
               <Typography
-                variant="h6"
                 sx={{
                   fontWeight: 'bold',
                   color: balance >= 0 ? '#4caf50' : '#f44336',
